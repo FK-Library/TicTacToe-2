9c849c648668ea007c1e4aae2eb541efee937d0c
FK-Library <fatemeh.khodadadi@outlook.com>
2022-12-06 22:15:16 +0000

    remove winner dto

--- TicTacToe22/TicTacToe22/Program.cs
+++ TicTacToe22/TicTacToe22/Program.cs
@@ -1,45 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel.DataAnnotations;
+
 using TicTacToe22;
 
 Console.WriteLine("Tic Tac Toe");
 string[][] board = { new string[] { "Z", "Z", "Z" }, new string[] { "Z", "Z", "Z" }, new string[] { "Z", "Z", "Z" } };
 
-var winner = new Winner();
-winner.Won = false;
+var winning = false;
 
 var inputService = new InputService();
 var display = new Display(board);
 
-
-while (!winner.Won)
+while (!winning)
 {
     display.DisplayBoard(board);
 
-
     //collect payer 1 & 2  input
     var details = inputService.CollectDetails();
-
-
-
     foreach (var item in details.Players)
     {
         var movements = new MovementService(item, board);
         var TupplePlayerAndBoard = movements.FillBoard(item, board);
         var applyRules = new ApplyRulesService(player: TupplePlayerAndBoard.Item2, board: TupplePlayerAndBoard.Item1);
-        winner.Won = applyRules.Game(player: TupplePlayerAndBoard.Item2, board: TupplePlayerAndBoard.Item1);
-        if (winner.Won)
+        winning = applyRules.Game(player: TupplePlayerAndBoard.Item2, board: TupplePlayerAndBoard.Item1);
+        if (winning)
         {
-            winner.Name = item.Name;
-            Console.WriteLine($"Winner is {winner.Name} ");
+            Console.WriteLine($"Winner is {item.Name} ");
             display.DisplayBoard(board);
             break;
-
-
         }
-
-
     }
-
 }
 Console.ReadKey();
